32725f4ade913a6243835d4de7754ea64c7d73e6
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2022-08-13 11:44:17 +0900

    support pm4

--- src/jojoe77777/VanillaXYZ/Main.php
+++ src/jojoe77777/VanillaXYZ/Main.php
@@ -6,17 +6,19 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\network\mcpe\protocol\GameRulesChangedPacket;
 use pocketmine\plugin\PluginBase;
+use pocketmine\network\mcpe\protocol\types\GameRule;
+use pocketmine\network\mcpe\protocol\types\IntGameRule;
 
 class Main extends PluginBase implements Listener {
 
-    public function onEnable(){
+    public function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onPlayerJoin(PlayerJoinEvent $ev){
         $pk = new GameRulesChangedPacket();
-        $pk->gameRules = ["showcoordinates" => [1, true]];
-        $ev->getPlayer()->dataPacket($pk);
+        $pk->gameRules = ["showcoordinates" => new IntGameRule(1, true)];
+        $ev->getPlayer()->getNetworkSession()->sendDataPacket($pk);
     }
 
 }
